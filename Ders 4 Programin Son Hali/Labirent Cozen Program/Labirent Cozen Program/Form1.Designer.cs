cf5118fc1d6a05f2c0fb66e1ae8be6fdbcc524a9
Furkan <114299899+FurcanY@users.noreply.github.com>
2023-09-04 23:42:38 +0300

    Son Ders Kodları
     projemin nasil yapildigini cekitigim youtube kanalimda son derste yazilan kodlarin ve projenin son halinin yuklenmesi

---
+++ Ders 4 Programin Son Hali/Labirent Cozen Program/Labirent Cozen Program/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Labirent_Cozen_Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            Bilgi_Lbl = new Label();
+            label1 = new Label();
+            Coz_Btn = new Button();
+            getir_btn = new Button();
+            kaydet_btn = new Button();
+            varsayilan_btn = new Button();
+            Butun_Hucre_Sil_Btn = new Button();
+            BitisKapisi_Koy = new Button();
+            BaslangicKapi_Koy_Btn = new Button();
+            Hucre_Sifirla_Btn = new Button();
+            DuvarKoy_Btn = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDarkDark;
+            panel1.Controls.Add(Bilgi_Lbl);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(Coz_Btn);
+            panel1.Controls.Add(getir_btn);
+            panel1.Controls.Add(kaydet_btn);
+            panel1.Controls.Add(varsayilan_btn);
+            panel1.Controls.Add(Butun_Hucre_Sil_Btn);
+            panel1.Controls.Add(BitisKapisi_Koy);
+            panel1.Controls.Add(BaslangicKapi_Koy_Btn);
+            panel1.Controls.Add(Hucre_Sifirla_Btn);
+            panel1.Controls.Add(DuvarKoy_Btn);
+            panel1.Dock = DockStyle.Right;
+            panel1.Location = new Point(660, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 659);
+            panel1.TabIndex = 0;
+            // 
+            // Bilgi_Lbl
+            // 
+            Bilgi_Lbl.AutoSize = true;
+            Bilgi_Lbl.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Bilgi_Lbl.Location = new Point(17, 9);
+            Bilgi_Lbl.Name = "Bilgi_Lbl";
+            Bilgi_Lbl.Size = new Size(162, 25);
+            Bilgi_Lbl.TabIndex = 2;
+            Bilgi_Lbl.Text = "Yerlestirme : Pasif";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(71, 587);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // Coz_Btn
+            // 
+            Coz_Btn.BackColor = Color.FromArgb(255, 128, 255);
+            Coz_Btn.FlatStyle = FlatStyle.Flat;
+            Coz_Btn.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            Coz_Btn.Location = new Point(26, 372);
+            Coz_Btn.Name = "Coz_Btn";
+            Coz_Btn.Size = new Size(153, 40);
+            Coz_Btn.TabIndex = 1;
+            Coz_Btn.Text = "Coz";
+            Coz_Btn.UseVisualStyleBackColor = false;
+            Coz_Btn.Click += Coz_Btn_Click;
+            // 
+            // getir_btn
+            // 
+            getir_btn.BackColor = Color.White;
+            getir_btn.FlatStyle = FlatStyle.Flat;
+            getir_btn.Location = new Point(26, 542);
+            getir_btn.Name = "getir_btn";
+            getir_btn.Size = new Size(153, 29);
+            getir_btn.TabIndex = 1;
+            getir_btn.Text = "Labirent Getir";
+            getir_btn.UseVisualStyleBackColor = false;
+            getir_btn.Click += getir_btn_Click;
+            // 
+            // kaydet_btn
+            // 
+            kaydet_btn.BackColor = Color.White;
+            kaydet_btn.FlatStyle = FlatStyle.Flat;
+            kaydet_btn.Location = new Point(26, 507);
+            kaydet_btn.Name = "kaydet_btn";
+            kaydet_btn.Size = new Size(153, 29);
+            kaydet_btn.TabIndex = 1;
+            kaydet_btn.Text = "Labirent Kaydet";
+            kaydet_btn.UseVisualStyleBackColor = false;
+            kaydet_btn.Click += kaydet_btn_Click;
+            // 
+            // varsayilan_btn
+            // 
+            varsayilan_btn.BackColor = Color.White;
+            varsayilan_btn.FlatStyle = FlatStyle.Flat;
+            varsayilan_btn.Location = new Point(26, 472);
+            varsayilan_btn.Name = "varsayilan_btn";
+            varsayilan_btn.Size = new Size(153, 29);
+            varsayilan_btn.TabIndex = 1;
+            varsayilan_btn.Text = "Varsayilan Labirent Getir";
+            varsayilan_btn.UseVisualStyleBackColor = false;
+            varsayilan_btn.Click += varsayilan_btn_Click;
+            // 
+            // Butun_Hucre_Sil_Btn
+            // 
+            Butun_Hucre_Sil_Btn.BackColor = Color.Red;
+            Butun_Hucre_Sil_Btn.FlatStyle = FlatStyle.Flat;
+            Butun_Hucre_Sil_Btn.Location = new Point(26, 203);
+            Butun_Hucre_Sil_Btn.Name = "Butun_Hucre_Sil_Btn";
+            Butun_Hucre_Sil_Btn.Size = new Size(153, 29);
+            Butun_Hucre_Sil_Btn.TabIndex = 1;
+            Butun_Hucre_Sil_Btn.Text = "Butun Hucreleri Sifirla";
+            Butun_Hucre_Sil_Btn.UseVisualStyleBackColor = false;
+            Butun_Hucre_Sil_Btn.Click += Butun_Hucre_Sil_Btn_Click;
+            // 
+            // BitisKapisi_Koy
+            // 
+            BitisKapisi_Koy.BackColor = Color.Lime;
+            BitisKapisi_Koy.FlatStyle = FlatStyle.Flat;
+            BitisKapisi_Koy.Location = new Point(26, 168);
+            BitisKapisi_Koy.Name = "BitisKapisi_Koy";
+            BitisKapisi_Koy.Size = new Size(153, 29);
+            BitisKapisi_Koy.TabIndex = 1;
+            BitisKapisi_Koy.Text = "Bitis Kapisi Koy";
+            BitisKapisi_Koy.UseVisualStyleBackColor = false;
+            BitisKapisi_Koy.Click += BitisKapisi_Koy_Click;
+            // 
+            // BaslangicKapi_Koy_Btn
+            // 
+            BaslangicKapi_Koy_Btn.BackColor = Color.Blue;
+            BaslangicKapi_Koy_Btn.FlatStyle = FlatStyle.Flat;
+            BaslangicKapi_Koy_Btn.Location = new Point(26, 133);
+            BaslangicKapi_Koy_Btn.Name = "BaslangicKapi_Koy_Btn";
+            BaslangicKapi_Koy_Btn.Size = new Size(153, 29);
+            BaslangicKapi_Koy_Btn.TabIndex = 1;
+            BaslangicKapi_Koy_Btn.Text = "Baslangic Kapisi Koy";
+            BaslangicKapi_Koy_Btn.UseVisualStyleBackColor = false;
+            BaslangicKapi_Koy_Btn.Click += BaslangicKapi_Koy_Btn_Click;
+            // 
+            // Hucre_Sifirla_Btn
+            // 
+            Hucre_Sifirla_Btn.BackColor = Color.White;
+            Hucre_Sifirla_Btn.FlatStyle = FlatStyle.Flat;
+            Hucre_Sifirla_Btn.Location = new Point(26, 98);
+            Hucre_Sifirla_Btn.Name = "Hucre_Sifirla_Btn";
+            Hucre_Sifirla_Btn.Size = new Size(153, 29);
+            Hucre_Sifirla_Btn.TabIndex = 1;
+            Hucre_Sifirla_Btn.Text = "Hucreyi Sifirla";
+            Hucre_Sifirla_Btn.UseVisualStyleBackColor = false;
+            Hucre_Sifirla_Btn.Click += Hucre_Sifirla_Btn_Click;
+            // 
+            // DuvarKoy_Btn
+            // 
+            DuvarKoy_Btn.BackColor = Color.FromArgb(128, 64, 64);
+            DuvarKoy_Btn.FlatStyle = FlatStyle.Flat;
+            DuvarKoy_Btn.Location = new Point(26, 63);
+            DuvarKoy_Btn.Name = "DuvarKoy_Btn";
+            DuvarKoy_Btn.Size = new Size(153, 29);
+            DuvarKoy_Btn.TabIndex = 1;
+            DuvarKoy_Btn.Text = "Duvar Koy";
+            DuvarKoy_Btn.UseVisualStyleBackColor = false;
+            DuvarKoy_Btn.Click += DuvarKoy_Btn_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDark;
+            ClientSize = new Size(860, 659);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "Labirent Cozum Programi";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button Butun_Hucre_Sil_Btn;
+        private Button BitisKapisi_Koy;
+        private Button BaslangicKapi_Koy_Btn;
+        private Button Hucre_Sifirla_Btn;
+        private Button DuvarKoy_Btn;
+        private Label label1;
+        private Label Bilgi_Lbl;
+        private Button Coz_Btn;
+        private Button getir_btn;
+        private Button kaydet_btn;
+        private Button varsayilan_btn;
+    }
+}
---
+++ Ders 4 Programin Son Hali/Labirent Cozen Program/Labirent Cozen Program/Labirent.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labirent_Cozen_Program
+{
+    internal class Labirent
+    {
+        private string VarsayilanLabirent= "00000000000000000000000000000111111111111111111111111110000000000000000000001000011201111111111111110100101101000000000000000001011110000101000001111111111101101001010000110001000000010110000101100001000100000001010010010100010101000111110101001001011100011111110001010100110001000000000000000010100001001011011111111111101010111100101000000000000010101000000010101011111110101010111111101000000000001010111010000000101101111110101010101010011100000000000010101010101001111111111111111010111011111000000010001000101011101111110111100010101010101110111110000010101010101010111011111011001010101010101000000010100100101010101010111111001010010010101010000000000000101111111000111111011111111111130000100010000001000000100111110010001001110101111110000000001000001000000000000001111110000";
+
+        private Label[,] hucreler = new Label[30, 30];
+        private int[,] veriler = new int[30, 30];
+        //mesela veriler[0,0] dizisine -1 atamak o hucrenin duvar oldugu anlamina gelir
+        //veriler[0,0] = 0 olursa o hucrenin bos bir hucre oldugu anlamina geliyor.
+
+        private int boyut = 20;
+        //her bir labelin genislik ve uzunluk bilgisini tutan degisken
+
+        private Color renk = Color.White;
+
+        private int bosluk = 2;
+        //her iki label arasi bosluk bilgisi
+
+        private int[] baslangicKapiVeri = new int[2] {0,0};
+        //baslangic kapisi koordinat labirent cozumu yapilirken gerekli olacak
+        private int[] bitisKapiVeri=new int[2] {0,0};
+        public Labirent()
+        {
+            //constructor
+            Labirent_Olustur();
+        }
+
+        private void Labirent_Olustur()
+        {
+            int kordinatx = 0;
+            int kordinaty = 0;
+            for (int x = 0; x < 30; x++)
+            {
+                for (int y = 0; y < 30; y++)
+                {
+                    if (x == 0 || x == 29 || y == 29 || y == 0)
+                    {
+                        veriler[x, y] = -1;
+                        //labirentin dis cercevesini duvar yapmak
+                    }
+                    else
+                    {
+                        veriler[x, y] = 0;
+                    }
+                    Label_Olustur(x, y, kordinatx, kordinaty);
+                    kordinatx = kordinatx + boyut + bosluk;
+                }
+                //her satir sonrasi x koordinati sifirlanir
+                kordinatx = 0;
+                kordinaty = kordinaty + boyut + bosluk;
+            }
+        }
+        private void Label_Olustur(int x, int y, int kordinatx, int kordinaty)
+        {
+            Label lbl = new Label();
+            lbl.Size = new Size(boyut, boyut);
+            lbl.Font = new Font("Arial", 6);
+            lbl.ForeColor = Color.Black;
+
+            lbl.Text = x.ToString() + " " + y.ToString();
+            //labelin hucreler dizisindeki indis bilgileri textine yazilir (daha sora bu bilgi ile label'a erisecegiz)
+            if (veriler[x, y] == -1)
+            {
+                
+                lbl.BackColor = Color.Black;
+                //labirentin dis cercevesini olusturmak icin siyah renk yaptik.
+            }
+            else
+            {
+                //renk= color.white
+                lbl.BackColor = renk;
+            }
+            lbl.Location = new Point(kordinatx, kordinaty);
+            hucreler[x, y] = lbl;
+            //en sonda hucreler adindaki lbl dizimizin dogru indexine labelimizi atiyoruz.
+        }
+        public Label Label_Getir(int x, int y)
+        {
+            return hucreler[x, y];
+        }
+        public void Labirent_Yerlestir(Form1 form)
+        {
+            for (int x = 0; x < 30; x++)
+            {
+                for (int y = 0; y < 30; y++)
+                {
+                    form.Controls.Add(hucreler[x,y]);
+                    //parametre olarak alinan forma olusturmus oldugumuz labelleri ekliyoruz.
+                }
+            }
+        }
+        public void Veri_Degistir(int x, int y, int veri)
+        {
+            //hucrenin verisi parametere olarak alinip bu method icerisinde hucrenin verisi degistirilir
+
+            if (veri == -2)
+            {
+                MessageBox.Show("Lutfen sag taraftan islem secini !");
+            }
+            else
+            {
+                //diger veri islemleri
+                if(veri == -1)
+                {
+                    //duvar verisi
+                    veriler[x, y] = veri;
+                    hucreler[x,y].BackColor=Color.Black;
+                    hucreler[x,y].ForeColor=Color.Black;
+                }
+                if (veri == 0)
+                {
+                    //bos hucre verisi
+                    veriler[x, y] = veri;
+                    hucreler[x, y].BackColor = Color.White;
+                    hucreler[x, y].ForeColor = Color.White;
+                }
+                if (veri == 2)
+                {
+                    //baslangic kapisi ise
+                    if (baslangicKapiVeri[0]!=0 )
+                    {
+                        veriler[baslangicKapiVeri[0], baslangicKapiVeri[1]] = 0;
+                        hucreler[baslangicKapiVeri[0], baslangicKapiVeri[1]].BackColor = Color.White;
+                        hucreler[baslangicKapiVeri[0], baslangicKapiVeri[1]].ForeColor = Color.White;
+                    }
+
+                    
+
+
+                    baslangicKapiVeri[0] = x;
+                    baslangicKapiVeri[1] = y;
+
+                    veriler[x, y] = veri;
+                    hucreler[x, y].BackColor = Color.Blue;
+                    hucreler[x, y].ForeColor = Color.Blue;
+                }
+                if(veri==3)
+                {
+                    //bitis kapisi ise
+                    if (bitisKapiVeri[0] != 0)
+                    {
+                        veriler[bitisKapiVeri[0], bitisKapiVeri[1]] = 0;
+                        hucreler[bitisKapiVeri[0], bitisKapiVeri[1]].BackColor = Color.White;
+                        hucreler[bitisKapiVeri[0], bitisKapiVeri[1]].ForeColor = Color.White;
+                    }
+                    
+
+
+
+                    bitisKapiVeri[0] = x;
+                    bitisKapiVeri[1] = y;
+
+
+                    veriler[x, y] = veri;
+                    hucreler[x, y].BackColor = Color.Green;
+                    hucreler[x, y].ForeColor = Color.Green;
+                }
+                
+
+            }
+           
+        }
+        public void Hucre_Veri_Sil()
+        {
+            for (int y = 1; y < 29; y++)
+            {
+                for (int x = 1; x < 29; x++)
+                {
+                    veriler[x, y] = 0;
+                    hucreler[x, y].BackColor = Color.White;
+                    hucreler[x, y].ForeColor = Color.White;
+
+                }
+            }
+            MessageBox.Show("Hucre Verileri Sifirlandi");
+        }
+        public bool KapilarVarMi()
+        {
+            if (baslangicKapiVeri[0] == 0 && baslangicKapiVeri[1] == 0 || bitisKapiVeri[0] == 0 && bitisKapiVeri[1] == 0)
+            {
+                return false;
+
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public int[] BaslangicKapi_Veri_Al()
+        {
+            return baslangicKapiVeri;
+        }
+        public int Labirent_Coz(int x, int y)
+        {
+            if (veriler[x, y] == 3)
+            {
+                return 1;
+                //1 degeri dondurmek sonuca ulasildigi anlamina geliyor
+            }
+            if (veriler[x,y]==0 || veriler[x, y] == 2)
+            {
+                if (veriler[x, y] != 2)
+                {
+                    hucreler[x, y].BackColor = Color.Yellow;
+                    hucreler[x, y].ForeColor = Color.Yellow;
+                }
+                veriler[x, y] = 1;
+                //artik bu hucreden gecildigi anlamina geliyor
+                
+                
+                if (Labirent_Coz(x + 1, y) == 1)
+                    return 1;
+                //sag hucrenin bos olup olmadigina bakilir
+                if (Labirent_Coz(x , y-1) == 1)
+                    return 1;
+                //yukari hucrenin bos olup olmadigina bakilir
+                if (Labirent_Coz(x-1, y ) == 1)
+                    return 1;
+                //sol hucrenin bos olup olmadigina bakilir
+                if (Labirent_Coz(x, y + 1) == 1)
+                    return 1;
+                //asagi hucrenin bos olup olmadigina bakilir
+                veriler[x, y] = 5;
+                hucreler[x, y].BackColor = Color.Red;
+                hucreler[x, y].ForeColor = Color.Red;
+
+
+            }
+            return 0;
+        }
+
+        public void Labirent_Getir(string labirent)
+        {
+            if (labirent == "")
+            {
+                labirent = VarsayilanLabirent;
+            }
+            int sayac = 0;
+            int veri;
+            for (int y = 1; y < 29; y++)
+            {
+                for (int x = 1; x < 29; x++)
+                {
+                    if (labirent[sayac] == '1')
+                    {
+                        veri = -1;
+                    }
+                    else
+                    {
+                        veri = (int)Char.GetNumericValue(labirent[sayac]);
+                    }
+                    Veri_Degistir(x,y,veri);
+                    sayac++;
+                }
+            }
+        }
+        public int Labirent_VeriAl(int x, int y)
+        {
+            return veriler[x, y];
+        }
+    }
+    
+}
